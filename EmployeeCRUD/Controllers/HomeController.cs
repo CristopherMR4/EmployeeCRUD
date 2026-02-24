d442a19039efc6e2a48049acb42a8c4499481c94
Cristopher MR <cristophermr04@gmail.com>
2026-02-24 19:57:36 -0400

    feat: se agrego la funcion de crear y guardar empleados

--- EmployeeCRUD/Controllers/HomeController.cs
+++ EmployeeCRUD/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 using System.Diagnostics;
 using EmployeeCRUD.Models;
 using Microsoft.AspNetCore.Mvc;
-//importanciones
+//importaciones
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using EmployeeCRUD.Model;
 //
+using EmployeeCRUD.Model.SubModel;
+using Microsoft.Identity.Client;
 
 
 namespace EmployeeCRUD.Controllers
@@ -24,6 +26,40 @@ namespace EmployeeCRUD.Controllers
             List<Employee>EmployeeList = _dbContext.Employees.Include(r => r.ObjRol).ToList();
             //Consulta a la BD para traer la data incluyyendo los datos relacionados de la tabla Rol
             return View(EmployeeList);
+        }
+
+        [HttpGet]
+        public IActionResult EmployeeDetail() {
+
+            //creation of an object that will help us to create a new employee in a safer way than directly
+            //affecting the main classes.
+            EmployeeVM oEmployeeVM = new EmployeeVM()
+            {
+
+                oEmployee = new Employee(),
+                oRolList = _dbContext.Rols.Select(rol => new SelectListItem()
+                {
+                    Text = rol.Detail,
+                    Value = rol.IdRol.ToString()
+                }).ToList(),
+                
+
+            };
+
+            return View(oEmployeeVM);        
+        }
+
+        [HttpPost]
+        public IActionResult EmployeeDetail(EmployeeVM oEmployeeVM) {
+
+            if(oEmployeeVM.oEmployee.IdEmployee == 0)
+            {
+                _dbContext.Employees.Add(oEmployeeVM.oEmployee);
+            }
+
+            _dbContext.SaveChanges();
+
+            return RedirectToAction("Index","Home");
         }
 
 
