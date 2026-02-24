b515c6643d419f51c58acd333ed0dbe3ff02fb8b
Cristopher MR <cristophermr04@gmail.com>
2026-02-24 19:54:35 -0400

    feat: creacion de la carpeta SubModel

---
+++ EmployeeCRUD/Model/SubModel/EmployeeVM.cs
@@ -0,0 +1,16 @@
+﻿//import
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+
+namespace EmployeeCRUD.Model.SubModel
+{
+    public class EmployeeVM
+    {
+        public Employee oEmployee {  get; set; }
+        //in a sfer way to use the values of Employee
+
+        public List<SelectListItem> oRolList { get; set; }
+        //this a list that will have the values of my table Rol
+
+    }
+}
